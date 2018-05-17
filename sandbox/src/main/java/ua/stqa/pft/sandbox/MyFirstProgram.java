c6d3266493fbe6e20fb286ad491edd4aa8ea7b80
vizv_mac <vizv@MBP-Victor.localdomain>
2018-05-17 10:04:20 +0300

    Created classes

--- sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,30 +1,26 @@
 package ua.stqa.pft.sandbox;
 
+import java.awt.*;
+
 public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
     hello("Viktor");
     hello("user");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-    
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Square s = new Square(5);
+   // s.l = 5;
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(4,6);
+//    r.a = 4;
+//    r.b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
 
 
   public static void hello(String somebody) {
     System.out.println("Hello " + somebody + "!");
-  }
-
-  public static double area (double len) {
-    return len * len;
-  }
-
-  public static double area (double a, double b) {
-    return a * b;
   }
 
 }
